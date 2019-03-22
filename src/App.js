6ec60fb6ae2da2df3896e5b022508413c5c9cda1
ryane <ryanesc@gmail.com>
2019-03-22 15:03:40 -0400

    add css baseline

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import Button from '@material-ui/core/Button'
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Button variant="contained" color="primary">
-          Kruise
-        </Button>
-      </div>
+      <React.Fragment>
+        <CssBaseline />
+        <div>
+          <Button variant="contained" color="primary">
+            Kruise
+          </Button>
+        </div>
+      </React.Fragment>
     );
   }
 }
