9beb0fcb5e4f6d680f2991afd4566d5291e0a7f5
ryane <ryanesc@gmail.com>
2019-03-22 22:23:10 -0400

    clean up placeholders

--- src/StepPlaceholder.js
+++ src/StepPlaceholder.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
 
 const styles = theme => ({
   root: {
     display: 'flex',
+  },
+  paper: {
+    padding: theme.spacing.unit * 2,
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
   },
 });
 
@@ -14,9 +21,15 @@ class StepPlaceholder extends Component {
 
     return (
       <div className={classes.root}>
-        <Typography component="h1" variant="h2" color="inherit" noWrap className={classes.title}>
-          { location.pathname }
-        </Typography>
+        <Grid container spacing={24}>
+          <Grid item xs={12}>
+            <Paper className={classes.paper}>
+              <Typography component="h1" variant="h2" color="inherit" noWrap className={classes.title}>
+                { location.pathname }
+              </Typography>
+            </Paper>
+          </Grid>
+        </Grid>
       </div>
     )
   }
