85060e7048e24deba24114d3ba61729f00fb6ee3
ryane <ryanesc@gmail.com>
2019-03-23 16:42:03 -0400

    use a single form on app details

--- src/AppDetails.js
+++ src/AppDetails.js
@@ -56,19 +56,6 @@ const regions = ["STL", "KCI", "BEL"];
 const next = "/service";
 
 class AppDetails extends Component {
-  /* state = {
-   *   appName: 'molly-data',
-   *   release: 'v1',
-   *   tenant: 'molly',
-   *   environment: 'Dev',
-   *   region: 'STL',
-   *   namespace: 'molly',
-   *   repoURL: 'github.com/ryane/molly-data',
-   *   path: '/',
-   *   targetRevision: "HEAD",
-   * };
-   */
-
   state = {
     name: '',
     release: '',
@@ -84,132 +71,133 @@ class AppDetails extends Component {
   handleChange = event => {
     this.setState({ [event.target.name]: event.target.value });
   };
-  };
 
   render() {
     const { classes } = this.props;
 
     return (
       <div className={classes.root}>
-        <Grid container spacing={24}>
-          <Grid item xs={5}>
-            <Paper className={classes.paper}>
-              <form className={classes.container} noValidate autoComplete="off">
-                <TextField
-                  name="name"
-                  label="Application Name"
-                  className={classes.textField}
-                  value={this.state.name}
-                  onChange={this.handleChange}
-                  margin="normal"
-                />
-                <TextField
-                  name="release"
-                  label="Release / Version"
-                  className={classes.textField}
-                  value={this.state.release}
-                  onChange={this.handleChange}
-                  margin="normal"
-                />
-                <TextField
-                  name="tenant"
-                  label="Team Organization"
-                  className={classes.textField}
-                  value={this.state.tenant}
-                  onChange={this.handleChange}
-                  margin="normal"
-                />
-                <FormControl className={classes.formControl}>
-                  <InputLabel htmlFor="environment">TargetEnvironment</InputLabel>
-                  <Select
-                    value={this.state.environment}
+        <form noValidate autoComplete="off">
+          <Grid container spacing={24}>
+            <Grid item xs={5}>
+              <Paper className={classes.paper}>
+                <div className={classes.container}>
+                  <TextField
+                    name="name"
+                    label="Application Name"
+                    className={classes.textField}
+                    value={this.state.name}
                     onChange={this.handleChange}
-                    inputProps={{
-                      name: 'environment',
-                      id: 'environment',
-                    }}
-                  >
-                    {envs.map((e) =>
-                      <MenuItem key={e} value={e}>{e}</MenuItem>
-                    )}
-                  </Select>
-                </FormControl>
-                <FormControl className={classes.formControl}>
-                  <InputLabel htmlFor="environment">Target Region</InputLabel>
-                  <Select
-                    value={this.state.region}
+                    margin="normal"
+                  />
+                  <TextField
+                    name="release"
+                    label="Release / Version"
+                    className={classes.textField}
+                    value={this.state.release}
                     onChange={this.handleChange}
-                    inputProps={{
-                      name: 'region',
-                      id: 'region',
-                    }}
-                  >
-                    {regions.map((e) =>
-                      <MenuItem key={e} value={e}>{e}</MenuItem>
-                    )}
-                  </Select>
-                </FormControl>
-                <TextField
-                  name="namespace"
-                  label="Target Namespace"
-                  className={classes.textField}
-                  value={this.state.namespace}
-                  onChange={this.handleChange}
-                  margin="normal"
-                />
-              </form>
-            </Paper>
+                    margin="normal"
+                  />
+                  <TextField
+                    name="tenant"
+                    label="Team Organization"
+                    className={classes.textField}
+                    value={this.state.tenant}
+                    onChange={this.handleChange}
+                    margin="normal"
+                  />
+                  <FormControl className={classes.formControl}>
+                    <InputLabel htmlFor="environment">TargetEnvironment</InputLabel>
+                    <Select
+                      value={this.state.environment}
+                      onChange={this.handleChange}
+                      inputProps={{
+                        name: 'environment',
+                        id: 'environment',
+                      }}
+                    >
+                      {envs.map((e) =>
+                        <MenuItem key={e} value={e}>{e}</MenuItem>
+                      )}
+                    </Select>
+                  </FormControl>
+                  <FormControl className={classes.formControl}>
+                    <InputLabel htmlFor="environment">Target Region</InputLabel>
+                    <Select
+                      value={this.state.region}
+                      onChange={this.handleChange}
+                      inputProps={{
+                        name: 'region',
+                        id: 'region',
+                      }}
+                    >
+                      {regions.map((e) =>
+                        <MenuItem key={e} value={e}>{e}</MenuItem>
+                      )}
+                    </Select>
+                  </FormControl>
+                  <TextField
+                    name="namespace"
+                    label="Target Namespace"
+                    className={classes.textField}
+                    value={this.state.namespace}
+                    onChange={this.handleChange}
+                    margin="normal"
+                  />
+                </div>
+              </Paper>
+            </Grid>
+            <Grid item xs={5}>
+              <Paper className={classes.paper}>
+                <div className={classes.container}>
+                  <TextField
+                    name="repoURL"
+                    label="Deployment Git Repo URL"
+                    className={classes.textField}
+                    value={this.state.repoURL}
+                    onChange={this.handleChange}
+                    margin="normal"
+                  />
+                  <TextField
+                    name="path"
+                    label="Deployment Git Path"
+                    className={classes.textField}
+                    value={this.state.path}
+                    onChange={this.handleChange}
+                    margin="normal"
+                  />
+                  <TextField
+                    name="targetRevision"
+                    label="Deployment Git Revision"
+                    className={classes.textField}
+                    value={this.state.targetRevision}
+                    onChange={this.handleChange}
+                    margin="normal"
+                  />
+                </div>
+              </Paper>
+            </Grid>
+            <Grid item xs={2}>
+              <Button
+                disabled={true}
+                className={classes.button}
+              >
+                Back
+              </Button>
+              <Button
+                size="large"
+                variant="contained"
+                color="primary"
+                className={classes.button}
+                fullWidth={false}
+                component={Link}
+                to={next}
+              >
+                Next
+              </Button>
+            </Grid>
           </Grid>
-          <Grid item xs={5}>
-            <Paper className={classes.paper}>
-              <form className={classes.container} noValidate autoComplete="off">
-                <TextField
-                  name="repoURL"
-                  label="Deployment Git Repo URL"
-                  className={classes.textField}
-                  value={this.state.repoURL}
-                  onChange={this.handleChange}
-                  margin="normal"
-                />
-                <TextField
-                  name="path"
-                  label="Deployment Git Path"
-                  className={classes.textField}
-                  value={this.state.path}
-                  onChange={this.handleChange}
-                  margin="normal"
-                />
-                <TextField
-                  name="targetRevision"
-                  label="Deployment Git Revision"
-                  className={classes.textField}
-                  value={this.state.targetRevision}
-                  onChange={this.handleChange}
-                  margin="normal"
-                />
-              </form>
-            </Paper>
-          </Grid>
-          <Grid item xs={2}>
-            <Button
-              disabled={true}
-              className={classes.button}
-            >
-              Back
-            </Button>
-            <Button
-              size="large"
-              variant="contained"
-              color="primary"
-              className={classes.button}
-              fullWidth={false}
-              component={Link}
-              to={next}
-            >
-              Next
-            </Button>
-          </Grid>
-        </Grid>
+        </form>
       </div>
     )
   }
