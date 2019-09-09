d1bfc2e118c1f6077877ee35366ba88acd38e674
ryane <ryanesc@gmail.com>
2019-09-09 10:28:14 -0400

    fix defaults

--- src/components/ServicePanel.js
+++ src/components/ServicePanel.js
@@ -68,7 +68,8 @@ function ServicePanel(props) {
           $push: [
             {
               name: "",
-              port: 8080
+              port: 8080,
+              targetPort: ""
             }
           ]
         }
